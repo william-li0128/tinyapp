9b8f3f46519e375ad03cc553afe620494e8670d7
William Li <110069721+william-li0128@users.noreply.github.com>
2022-10-27 23:05:50 -0700

    Header Partial added and applied to the show and the index partials

--- express_server.js
+++ express_server.js
@@ -13,6 +13,17 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id: id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
